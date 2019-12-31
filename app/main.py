c751b6df3a9d451869cda91e10e8a01759e85ad9
coder <ngochieu10041997@gmail.com>
2019-12-31 09:35:48 +0700

    Add log

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
 
 import os
+import json
 import pandas as pd
 import traceback
 import requests
@@ -53,14 +54,15 @@ def getPhase_params(
         response = requests.get(api_backend, payload)
         deviceLog_df = pd.DataFrame.from_dict(response.json()["data"])
 
-        print(
-            "\n\n\nYAYYYYYYYY! GOT THE DATAFRAME FROM BACKEND, shape of it: ",
-            deviceLog_df.shape,
-        )
-        print("DATAFRAME COLUMNS: ", deviceLog_df.columns)
-
         # Fake data
         deviceLog_df = pd.read_csv("./DeviceLog.csv")
+
+        print(
+            "\n\n\nGOT THE DATAFRAME FROM BACKEND, shape: ", deviceLog_df.shape,
+        )
+        print("\nDATAFRAME COLUMNS: ", deviceLog_df.columns)
+        # print("\nDATAFRAME")
+        # print(deviceLog_df.to_string())
 
     except Exception as e:
         print("Type Error: ", e)
@@ -94,6 +96,7 @@ def getPhase_params(
 
     # Get Phase
     try:
+        print("Retrieve phase 0 data...")
         phase0_df = service.getClusterDataframe(
             start_time=phase0_startTime, end_time=phase0_endTime, dataframe=selected_df,
         )
@@ -103,6 +106,7 @@ def getPhase_params(
         return {"error": "Error when trying to load phase 0 data", "errstr": e}
 
     try:
+        print("\nProcessing phase 0...")
         phase0_A, phase0_B = calculate.getAB_fromDevice(
             y_device_mac=photo_table_mac,
             x_device_mac=photo_faceup_mac,
@@ -117,6 +121,7 @@ def getPhase_params(
         }
 
     try:
+        print("Retrieve phase 1 data...")
         phase1_df = service.getClusterDataframe(
             start_time=phase1_startTime, end_time=phase1_endTime, dataframe=selected_df,
         )
@@ -126,6 +131,7 @@ def getPhase_params(
         return {"error": "Error when trying to load phase 1 data", "errstr": e}
 
     try:
+        print("\nProcessing phase 1...")
         phase1_A, phase1_B = calculate.getAB_fromDevice(
             y_device_mac=photo_table_mac,
             x_device_mac=light_down_mac,
@@ -161,6 +167,7 @@ calAB_parser.add_argument("environment", type=str, required=False)
 
 class calculate_AB(Resource):
     def get(self):
+        print("\n\nGET")
         args = calAB_parser.parse_args()
 
         LIGHT_DOWN_MAC = args["light_down_mac"]
@@ -216,7 +223,10 @@ class calculate_AB(Resource):
             print(traceback.format_exc())
             return {"error": "Error when trying to load calculate A B", "errstr": e}
 
-        return {"A": A, "B": B}
+        # Return result
+        result = {"A": A, "B": B}
+        print("result: ", json.dumps(result, indent=1))
+        return result, 200
 
     def post(self):
         print("\n\nPOST")
@@ -236,6 +246,18 @@ class calculate_AB(Resource):
         setPoint = args["setPoint"]
         environment = args["environment"]
 
+        # Check logs
+        print("Light down Mac: ", LIGHT_DOWN_MAC)
+        print("Photo table Mac: ", PHOTO_TABLE_MAC)
+        print("Photo facedown Mac: ", PHOTO_FACEDOWN_MAC)
+        print("Photo faceup Mac: ", PHOTO_FACEUP_MAC)
+        print("phase 0 start time: ", phase0_startTime)
+        print("phase 0 end time: ", phase0_endTime)
+        print("phase 1 start time: ", phase1_startTime)
+        print("phase 1 end time: ", phase1_endTime)
+        print("set point: ", setPoint)
+        print("environment: ", environment)
+
         selected_devices_mac = [
             LIGHT_DOWN_MAC,
             PHOTO_TABLE_MAC,
@@ -276,8 +298,10 @@ class calculate_AB(Resource):
             print(traceback.format_exc())
             return {"error": "Error when trying to load calculate A B", "errstr": e}
 
-        return {"A": A, "B": B}, 200
-
+        # Return result
+        result = {"A": A, "B": B}
+        print("result: ", json.dumps(result, indent=1))
+        return result, 200
 
 calDim_parser = calAB_parser.copy()
 calDim_parser.add_argument("upValue", type=int, required=True)
